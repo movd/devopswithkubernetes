3d42e57ec19db6178596f5324b9a721efd7fe4dc
Moritz <22394314+movd@users.noreply.github.com>
2020-07-09 13:57:47 +0200

    Finish: Ex. 1.07: External access with Ingress

--- hashgenerator/index.js
+++ hashgenerator/index.js
@@ -1,11 +1,29 @@
 const { uuid } = require("uuidv4");
+const express = require("express");
+const app = express();
+
+const PORT = 3000;
+
+app.set("trust proxy", true);
+
+let hashTimestamp;
 
 const infiniteLoop = () => {
   setTimeout(() => {
     const ts = new Date();
-    console.log(`${ts.toISOString()}: ${uuid()}`);
+    hashTimestamp = `${ts.toISOString()}: ${uuid()}`;
+    console.log(hashTimestamp);
     infiniteLoop();
   }, 5000);
 };
 
 infiniteLoop();
+
+app.get("/", function (req, res) {
+  console.log(`${req.ip} requested timestamp`);
+  return res.send(hashTimestamp);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server started on: ${PORT}`);
+});
