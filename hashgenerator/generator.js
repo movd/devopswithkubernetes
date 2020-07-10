ad8cfcadb10d14ae8ca95f8f488ed8958e0cd9a8
Moritz <22394314+movd@users.noreply.github.com>
2020-07-10 15:06:04 +0200

    Finish: Ex. 1.10

---
+++ hashgenerator/generator.js
@@ -0,0 +1,37 @@
+require("dotenv").config();
+const { uuid } = require("uuidv4");
+const fsPromises = require("fs").promises;
+
+const { HASHFILE_PATH } = process.env;
+
+const writeHashFile = async (path) => {
+  let ts = new Date();
+  let hashTimestamp = `${ts.toISOString()}: ${uuid()}`;
+  try {
+    await fsPromises.writeFile(path, hashTimestamp);
+    console.log(`generated a new hash: ${hashTimestamp}`);
+    return hashTimestamp;
+  } catch (error) {
+    console.error(`error: ${error.message}`);
+  }
+};
+
+let i = 0;
+const hashLoop = () => {
+  setTimeout(async () => {
+    const generatedHashTimestamp = await writeHashFile(HASHFILE_PATH);
+    if (i < 100) {
+      hashLoop();
+    }
+    i++;
+  }, 5000);
+};
+
+(async () => {
+  try {
+    const generatedHashTimestamp = await writeHashFile(HASHFILE_PATH);
+    hashLoop();
+  } catch (error) {
+    console.error(`error: ${error.message}`);
+  }
+})();
--- hashgenerator/server.js
+++ hashgenerator/server.js
@@ -1,27 +1,23 @@
-const { uuid } = require("uuidv4");
+require("dotenv").config();
+const fsPromises = require("fs").promises;
 const express = require("express");
 const app = express();
+
+const { HASHFILE_PATH } = process.env;
 
 const PORT = 3000;
 
 app.set("trust proxy", true);
 
-let hashTimestamp;
-
-const infiniteLoop = () => {
-  setTimeout(() => {
-    const ts = new Date();
-    hashTimestamp = `${ts.toISOString()}: ${uuid()}`;
-    console.log(hashTimestamp);
-    infiniteLoop();
-  }, 5000);
-};
-
-infiniteLoop();
-
-app.get("/", function (req, res) {
-  console.log(`${req.ip} requested timestamp`);
-  return res.send(hashTimestamp);
+app.get("/", async (req, res) => {
+  console.log(`${req.ip} requested a hashFile`);
+  const hashFile = await fsPromises.readFile(HASHFILE_PATH, "utf-8");
+  try {
+    res.send(hashFile);
+  } catch (error) {
+    res.send(`error: ${error.message}`);
+    console.error(`error: ${error.message}`);
+  }
 });
 
 app.listen(PORT, () => {
