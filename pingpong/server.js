b139e5c4708800e268ab7e33c301b874d999344f
Moritz <22394314+movd@users.noreply.github.com>
2020-07-14 21:01:32 +0200

    Add: ping/pong app writing to disk is toggleable

--- pingpong/server.js
+++ pingpong/server.js
@@ -5,6 +5,9 @@ const fsPromises = require("fs").promises;
 const app = express();
 
 const { PINGPONGFILE_PATH } = process.env;
+if (PINGPONGFILE_PATH) console.log("PINGPONGFILE_PATH:", PINGPONGFILE_PATH);
+if (!PINGPONGFILE_PATH) console.log("Writing to txt is disabled");
+
 const PORT = 3000;
 
 app.set("trust proxy", true);
@@ -12,16 +15,26 @@ app.set("trust proxy", true);
 let counter = 0;
 
 app.get("/pingpong", async (req, res) => {
-  counter++;
-  console.log(
-    `pingpong: ${req.ip} has pinged. i have been ping ${counter} times.`
-  );
-  try {
-    await fsPromises.writeFile(PINGPONGFILE_PATH, `Ping / Pongs: ${counter}`);
-  } catch (error) {
-    console.error(`pingpong error: ${error.message}`);
+  // dont update if read-only header is set
+  if (!req.get("read-only")) {
+    counter++;
+    console.log(
+      `pingpong: ${req.ip} has pinged me. i have been ping ${counter} times.`
+    );
+  } else {
+    console.log(`pingpong: ${req.ip} has read my ping count`);
   }
-  return res.send(`pong ${counter}`);
+
+  // only write to disk if PINGPONGFILE_PATH is truthy
+  if (PINGPONGFILE_PATH) {
+    try {
+      await fsPromises.writeFile(PINGPONGFILE_PATH, `Ping / Pongs: ${counter}`);
+      console.log(`wrote to ${PINGPONGFILE_PATH}`);
+    } catch (error) {
+      console.error(`pingpong error: ${error.message}`);
+    }
+  }
+  return res.status(200).json({ counter });
 });
 
 app.listen(PORT, () => {
