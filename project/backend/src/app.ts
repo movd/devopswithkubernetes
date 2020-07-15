e6e216998d0459c64d5a816b816efbdbca9fbf6c
Moritz <22394314+movd@users.noreply.github.com>
2020-07-15 18:48:45 +0200

    Fix: remove redirect

--- project/backend/src/app.ts
+++ project/backend/src/app.ts
@@ -8,8 +8,4 @@ app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 app.use('/todos', todosRouter);
 
-app.get('/', (_req, res) => {
-  res.redirect('/todos');
-});
-
 export default app;
