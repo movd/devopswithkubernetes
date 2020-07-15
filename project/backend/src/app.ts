4991d5e7978638d8e28b983c4a7593525f89c504
Moritz <22394314+movd@users.noreply.github.com>
2020-07-15 15:42:27 +0200

    Fix: cors

--- project/backend/src/app.ts
+++ project/backend/src/app.ts
@@ -1,8 +1,9 @@
 import express from 'express';
-
+import cors from 'cors';
 import todosRouter from './routes/todos';
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 app.use('/todos', todosRouter);
