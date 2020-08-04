994fcf4576b6e42200dcceea53981b6bf4c6bb28
Moritz <22394314+movd@users.noreply.github.com>
2020-08-04 16:20:27 +0200

    Add: health check route

--- project/backend/src/app.ts
+++ project/backend/src/app.ts
@@ -3,6 +3,7 @@ import cors from 'cors';
 import morgan from 'morgan';
 
 import todosRouter from './routes/todos';
+import healthyRouter from './routes/healthy';
 
 const app = express();
 app.set('trust proxy', 'uniquelocal');
@@ -12,5 +13,6 @@ app.use(express.json());
 app.use(morgan('short'));
 app.use('/api/static', express.static(`${__dirname}/public`));
 app.use('/api/todos', todosRouter);
+app.use('/api/healthy', healthyRouter);
 
 export default app;
