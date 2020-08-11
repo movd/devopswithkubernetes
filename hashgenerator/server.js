6f090396b6edf7d0e2de6970ea19662fdb8878bd
Moritz <22394314+movd@users.noreply.github.com>
2020-08-11 12:24:37 +0200

    Add: health check

--- hashgenerator/server.js
+++ hashgenerator/server.js
@@ -5,16 +5,9 @@ const axios = require("axios");
 
 const app = express();
 
-const {
-  HASHFILE_PATH,
-  PINGPONGFILE_PATH,
-  PINGPONG_URL,
-  HASHGENERATOR_URL,
-} = process.env;
+const { PINGPONG_URL, HASHGENERATOR_URL } = process.env;
 console.log("HASHGENERATOR_URL:", HASHGENERATOR_URL);
-console.log("HASHFILE_PATH:", HASHFILE_PATH);
 console.log("PINGPONG_URL:", PINGPONG_URL);
-console.log("PINGPONGFILE_PATH:", PINGPONGFILE_PATH);
 
 const PORT = process.env.SERVER_PORT || 3000;
 const MESSAGE = process.env.MESSAGE || "Not from dotenv";
@@ -26,7 +19,7 @@ app.get("/", async (req, res) => {
 
   let currentHash;
 
-  if (!HASHGENERATOR_URL && !HASHFILE_PATH) {
+  if (!HASHGENERATOR_URL) {
     return res.status(400).send(`error not hash provider given`);
   }
 
@@ -42,10 +35,6 @@ app.get("/", async (req, res) => {
       };
 
       currentHash = await fetchHashes(HASHGENERATOR_URL);
-    }
-
-    if (HASHFILE_PATH) {
-      currentHash = await fsPromises.readFile(HASHFILE_PATH, "utf-8");
     }
 
     if (PINGPONG_URL) {
@@ -64,21 +53,39 @@ app.get("/", async (req, res) => {
         .send(`${MESSAGE}<br> ${currentHash}<br> ${pingPongCounter}`);
     }
 
-    if (PINGPONGFILE_PATH) {
-      const pingPongFile = await fsPromises.readFile(
-        PINGPONGFILE_PATH,
-        "utf-8"
-      );
-      return res
-        .status(200)
-        .send(`${MESSAGE}<br> ${currentHash}<br> ${pingPongFile}`);
-    }
-
     // If nothing
     return res.status(200).send(currentHash);
   } catch (error) {
     res.status(400).send(`error: ${error.message}`);
     console.error(`error: ${error.message}`);
+  }
+});
+
+app.get("/healthz", async (req, res) => {
+  if (!PINGPONG_URL)
+    return res.status(400).json({ error: "PINGPONG_URL is undefined" });
+  if (!HASHGENERATOR_URL)
+    return res.status(400).json({ error: "HASHGENERATOR_URL is undefined" });
+  const pingPongHealthz = `${new URL(PINGPONG_URL).origin}/healthz`; // http://localhost:3000/healthz
+
+  try {
+    const resAxios = await axios.get(pingPongHealthz);
+    if (resAxios.status === 200) {
+      return res.status(200).json({
+        [pingPongHealthz]: {
+          response: resAxios.data,
+          status: resAxios.status,
+        },
+      });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(400).json({
+      [pingPongHealthz]: {
+        response: error.message,
+        status: error.code,
+      },
+    });
   }
 });
 
