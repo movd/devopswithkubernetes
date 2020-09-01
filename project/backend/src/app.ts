27f5829dd8f6c6a5cc48c0b0e5c29b1dd9167ce1
Moritz <22394314+movd@users.noreply.github.com>
2020-09-01 11:40:01 +0200

    Add: catch wrong json syntax

--- project/backend/src/app.ts
+++ project/backend/src/app.ts
@@ -9,7 +9,18 @@ const app = express();
 app.set('trust proxy', 'uniquelocal');
 
 app.use(cors());
-app.use(express.json());
+// Configure to catch wrong json syntax
+app.use(
+  express.json({
+    verify: (_req, res: express.Response, buf, _encoding) => {
+      try {
+        JSON.parse(buf.toString());
+      } catch (e) {
+        res.status(400).json({ error: 'Invalid JSON' });
+      }
+    },
+  })
+);
 app.use(morgan('short'));
 app.use('/api/static', express.static(`${__dirname}/public`));
 app.use('/api/todos', todosRouter);
